1ad29a24716e4a1b549e8f37243bc7300b6158fa
mgaldino <mcz.fea@gmail.com>
2017-01-17 12:07:10 -0200

    Removi Joins (foi pra aula 4)

--- Aula_3.R
+++ Aula_3.R
@@ -296,45 +296,6 @@ View(top_status)
 
 ### Tempo por pedido
 
-### Joins
-## O dplyr também tem verbos para duas tabelas
-## os joins, que servem para juntar duas tabelas
-## Simples exemplo, adaptaod de https://stat545-ubc.github.io/bit001_dplyr-cheatsheet.html
-
-super_heroes <-
-  c("    name, alignment, gender,         publisher",
-    " Magneto,       bad,   male,            Marvel",
-    "   Storm,      good, female,            Marvel",
-    "Mystique,       bad, female,            Marvel",
-    "  Batman,      good,   male,                DC",
-    "   Joker,       bad,   male,                DC",
-    "Catwoman,       bad, female,                DC",
-    " Hellboy,      good,   male, Dark Horse Comics")
-super_heroes <- read.csv(text = super_heroes, strip.white = TRUE)
-
-publishers <- 
-  c("publisher, yr_founded",
-    "       DC,       1934",
-    "   Marvel,       1939",
-    "    Image,       1992")
-publishers <- read.csv(text = publishers, strip.white = TRUE)
-
-super_heroes
-publishers
-
-## objetivo, pegar o ano de fundaçao da revista, e juntar cm superheroes
-
-## verbos
-## inner_join, left_join, right_join, full_join etc.
-inner_join(super_heroes, publishers)
-# perdemos Hell boy
-
-left_join(super_heroes, publishers)
-
-right_join(super_heroes, publishers)
-
-anti_join(super_heroes, publishers)
-# deixa o que n tem match
 
 
 ## More resources

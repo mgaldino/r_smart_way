2d2ce8db2766303e6ceb4b5101bdc19812a0de58
mgaldino <mcz.fea@gmail.com>
2017-10-26 12:17:42 -0200

    Aula de anaálise de microdados - exemplo Anac

---
+++ aula_microdados.R
@@ -0,0 +1,8 @@
+## aula importação e manipulação de dados
+
+# A Anac disponibilizou microdados de tarifas aéreas (por mÊs)
+# https://sistemas.anac.gov.br/sas/downloads/view/frmDownload
+
+# vamos mexer nesses dados?
+
+setwd('C:\\Users\\mgaldino\\2017\\Geral TB\\Treinamento\\R\\r_smart_way\\Arquivos\\anac_12152211')
